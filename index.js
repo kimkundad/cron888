3ae9eb5efa77e727663530ee06b8931e22362ef3
kimkunndad <kim.kundad@gmail.com>
2021-08-02 00:31:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080
+const http = require('http');
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`)
+})
+
+var cron = require('node-cron');
+cron.schedule('*/5 * * * *', function(){
+   console.log('running a task every two minutes');
+
+   http.get('https://www.xn--888-4nlyax.com/lotto-admin/action/yeekee_runtime_setup.php', (res) => {
+    let rawHtml = '';
+    res.on('data', (chunk) => { rawHtml += chunk; });
+    res.on('end', () => {
+        try {
+            console.log(rawHtml);
+        } catch (e) {
+            console.error(e.message);
+        }
+    });
+});
+
+   http.get('https://www.xn--888-4nlyax.com/lotto-admin/action/yeekee_runtime.php', (res) => {
+    let rawHtml = '';
+    res.on('data', (chunk) => { rawHtml += chunk; });
+    res.on('end', () => {
+        try {
+            console.log(rawHtml);
+        } catch (e) {
+            console.error(e.message);
+        }
+    });
+});
+
+});
+
+module.exports = app
+console.log('Hello World')

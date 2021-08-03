8eaf31c1daad3ee493b8afe74052ee32319ecd16
kimkunndad <kim.kundad@gmail.com>
2021-08-04 01:36:26 +0700

    edit

--- cron.js
+++ cron.js
@@ -1,16 +1,16 @@
 
 
-const https = require('https');
+const http = require('http');
 const crypto = require("crypto");
 
 
 var cron = require('node-cron');
-cron.schedule('*/5 * * * *', function(){
+cron.schedule('*/2 * * * *', function(){
    console.log('running a task every two minutes');
 
    var n = crypto.randomInt(0, 1000000);
 
-   https.get('https://www.xn--888-4nlyax.com/lotto-admin/action/yeekee_runtime_setup.php?v'+n, (res) => {
+   http.get('http://localhost/tiger/lotto-admin/action/yeekee_runtime_setup.php?v'+n, (res) => {
     let rawHtml = '';
     res.on('data', (chunk) => { rawHtml += chunk; });
     res.on('end', () => {
@@ -22,7 +22,7 @@ cron.schedule('*/5 * * * *', function(){
     });
 });
 
-    https.get('https://www.xn--888-4nlyax.com/lotto-admin/action/yeekee_runtime.php?v'+n, (res) => {
+    http.get('http://localhost/tiger/lotto-admin/action/yeekee_runtime.php?v'+n, (res) => {
     let rawHtmls = '';
     res.on('data', (chunk) => { rawHtmls += chunk; });
     res.on('end', () => {
@@ -36,7 +36,7 @@ cron.schedule('*/5 * * * *', function(){
 
 
 
-https.get('https://www.xn--888-4nlyax.com/lotto-admin/action/webservice_runtime.php?v'+n, (res) => {
+http.get('http://localhost/tiger/lotto-admin/action/webservice_runtime.php?v'+n, (res) => {
     let rawHtml2 = '';
     res.on('data', (chunk) => { rawHtml2 += chunk; });
     res.on('end', () => {
